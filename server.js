e15cefe776f7e6e768387f514eb453d7e58d6fd4
Sunisita <suniista18@gmail.com>
2017-07-11 16:57:20 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('Privateroute hit');
+		next();
+	},
+	logger: function (req, res, next){
+		var d = new Date().toString();
+        console.log('Request: ' + 'Date:  ' + d + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+	}
+};
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+	res.send('About US');
+});
+
+app.use(express.static(__dirname  + '/public'));
+
+app.listen(PORT, function() {
+	console.log('Express Server started on port' + PORT + '!');
+});
